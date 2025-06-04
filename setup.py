9bbc23eef77faae6e1aa5483fcb638efc85dbea0
shauryaRaj <shaurya221340@gmail.com>
2025-06-05 01:47:40 +0530

    project strucutre ready, env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shaurya Raj",#changed original code
+    author_email="shaurya0805@gmail.com", #Changed original code
+    packages=find_packages()
+)
